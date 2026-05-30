8d441808980aa798f7f08b21f5627b809e0942f6
aoehlerewu <166942126+aoehlerewu@users.noreply.github.com>
2026-05-29 17:38:44 -0700

    fixed the coin saving
    should save coins on death, doesnt work if you quit to menu thuogh

--- Scripts/SceneScripts/GameOver.cs
+++ Scripts/SceneScripts/GameOver.cs
@@ -6,9 +6,11 @@ public partial class GameOver : CanvasLayer
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
-		GetNode<Button>("Control/RestartButton").Pressed +=OnRestartPressed;
-		GetNode<Button>("Control/TitleScreenButton").Pressed += OnTitleScreenPressed;
+	// Save coins when the player dies
+	GetNode<SaveSystem>("/root/SaveSystem").Save();
+	
+	GetNode<Button>("Control/RestartButton").Pressed += OnRestartPressed;
+	GetNode<Button>("Control/TitleScreenButton").Pressed += OnTitleScreenPressed;
 	}
 
 	private void OnRestartPressed(){
@@ -18,8 +20,9 @@ public partial class GameOver : CanvasLayer
 		GetTree().ChangeSceneToFile("res://Scenes/Level1.tscn");
 	}
 	
-	private void OnTitleScreenPressed(){
-		//Hook up the same way as above when the title screen is created 
-		GetTree().ChangeSceneToFile("res://Scenes/Menus/MainMenu.tscn");
+	private void OnTitleScreenPressed()
+	{
+	GetNode<SaveSystem>("/root/SaveSystem").Save();
+	GetTree().ChangeSceneToFile("res://Scenes/Menus/MainMenu.tscn");
 	}
 }
